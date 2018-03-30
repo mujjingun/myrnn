e77748799d0163a35b2977a94c6815205d6f4217
Gun Park <mujjingun@gmail.com>
2018-03-30 14:38:35 +0900

    add batch dimension to mock data

--- train.py
+++ train.py
@@ -9,15 +9,15 @@ features = tf.placeholder(tf.uint8, shape = (None, None)) # (B, N_f)
 loss = build_model(input_data, features)
 train_op = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
-BATCH_SIZE = 1
+BATCH_SIZE = 2
 
 with tf.Session(config=tf.ConfigProto()) as sess:
     sess.run([tf.global_variables_initializer()])
 
     for iterations in range(1000):
         mock_sentence = np.random.randint(0, 30, size=(BATCH_SIZE, 100))
-        mock_samples_c = np.expand_dims(np.repeat(mock_sentence, 10), 0)
-        mock_samples_f = mock_samples_c + np.expand_dims(np.repeat(np.expand_dims(np.arange(10), 0), 100, axis=0).flatten(), 0)
+        mock_samples_c = np.repeat(mock_sentence, 10, axis=1)
+        mock_samples_f = mock_samples_c + np.tile(np.repeat(np.expand_dims(np.arange(10), 0), 100, axis=0).flatten(), (BATCH_SIZE, 1))
         _, L = sess.run([train_op, loss],
                  {
                      input_data.name: np.stack([mock_samples_c, mock_samples_f], 2),
