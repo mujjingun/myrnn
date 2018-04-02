a12417c66f61ce3d04af0984e597a9bcf0d546cd
Gun Park <mujjingun@gmail.com>
2018-04-02 22:48:56 +0900

    add wave trainer

--- indrnn_convattention.py
+++ indrnn_convattention.py
@@ -7,9 +7,11 @@ from sample_cell import SampleCell
 
 def build_model(input_data, # (B, T, 2) uint8
                 features, # (B, N_f) uint8
-                DICT_SIZE = 30,
-                TIME_STEPS = 1000,
+                BATCH_SIZE,
+                DICT_SIZE,
+                TIME_STEPS,
                 NUM_UNITS = 256):
+
     # (B, T, coarse + fine)
     input_norm = tf.scalar_mul(1 / 256, tf.cast(input_data, tf.float32))
     # input data is now in [0, 1]
@@ -24,34 +26,62 @@ def build_model(input_data, # (B, T, 2) uint8
     # TODO: try the gradient punishing method used in improved gan
     RECURRENT_MAX = pow(2, 1 / TIME_STEPS)
 
-    encoder_outputs, _ = tf.nn.dynamic_rnn(
-          tf.contrib.rnn.MultiRNNCell(
-            [IndRNNCell(NUM_UNITS, recurrent_max_abs=RECURRENT_MAX)]),
-          features_1h, dtype=tf.float32)
+    # Sentence Encoder RNN
+    encoder_cell = tf.contrib.rnn.MultiRNNCell(
+           [IndRNNCell(NUM_UNITS, recurrent_max_abs=RECURRENT_MAX)])
+
+    enc_init_state = encoder_cell.zero_state(dtype=tf.float32, batch_size=BATCH_SIZE)
+
+    encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
+          encoder_cell,
+          features_1h,
+          initial_state=enc_init_state,
+          dtype=tf.float32)
     
+    # Attention RNN
     att_cell = tf.contrib.seq2seq.AttentionWrapper(
           IndRNNCell(NUM_UNITS, recurrent_max_abs=RECURRENT_MAX),
           tf.contrib.seq2seq.BahdanauAttention(NUM_UNITS, encoder_outputs, normalize=True),
           attention_layer_size=NUM_UNITS)
 
-    attention, _ = tf.nn.dynamic_rnn(att_cell, cf_tm1, dtype=tf.float32)    
+    att_init_state = att_cell.zero_state(dtype=tf.float32, batch_size=BATCH_SIZE)
+
+    attention, att_state = tf.nn.dynamic_rnn(
+          att_cell, 
+          cf_tm1, 
+          initial_state=att_init_state, 
+          dtype=tf.float32)    
 
     # rnn cell for predicting coarse
-    cell = tf.contrib.rnn.MultiRNNCell(
+    c_cell = tf.contrib.rnn.MultiRNNCell(
             [tf.contrib.rnn.ResidualWrapper(IndRNNCell(NUM_UNITS, recurrent_max_abs=RECURRENT_MAX, name="cell00")),
              tf.contrib.rnn.ResidualWrapper(IndRNNCell(256, recurrent_max_abs=RECURRENT_MAX, name="cell01"))])
-    coarse_logits, _ = tf.nn.dynamic_rnn(cell, attention, dtype=tf.float32)
+    
+    c_init_state = c_cell.zero_state(dtype=tf.float32, batch_size=BATCH_SIZE)
+
+    coarse_logits, c_state = tf.nn.dynamic_rnn(
+          c_cell, 
+          attention, 
+          initial_state=c_init_state,
+          dtype=tf.float32)
 
     # rnn cell for predicting fine
-    cell2 = tf.contrib.rnn.MultiRNNCell(
+    f_cell = tf.contrib.rnn.MultiRNNCell(
             [tf.contrib.rnn.ResidualWrapper(IndRNNCell(NUM_UNITS, recurrent_max_abs=RECURRENT_MAX, name="cell10")),
              tf.contrib.rnn.ResidualWrapper(IndRNNCell(256, recurrent_max_abs=RECURRENT_MAX, name="cell11"))])
 
     # c_t-1 + f_t-1 + c_t
-    cell2_input = attention + input_norm[:, 1:, :1]
-    fine_logits, _ = tf.nn.dynamic_rnn(cell2, cell2_input, dtype=tf.float32)
+    f_input = attention + input_norm[:, 1:, :1]
 
-    # TODO: Original paper uses MSE
+    f_init_state = f_cell.zero_state(dtype=tf.float32, batch_size=BATCH_SIZE)
+
+    fine_logits, f_state = tf.nn.dynamic_rnn(
+          f_cell, 
+          f_input, 
+          initial_state=f_init_state,
+          dtype=tf.float32)
+
+    # Calculate loss and accuracy
     c_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
         labels=input_data[:, 1:, 0], logits=coarse_logits))
 
@@ -70,6 +100,12 @@ def build_model(input_data, # (B, T, 2) uint8
         tf.argmax(fine_logits, axis=2, output_type=tf.int32)
         ), tf.float32))
 
-    dist = tf.concat([tf.nn.softmax(fine_logits), tf.nn.softmax(coarse_logits)], 2)
+    # probability distribution for introspection
+    prs = tf.concat([tf.nn.softmax(fine_logits), tf.nn.softmax(coarse_logits)], 2)
 
-    return c_loss, f_loss, c_accuracy, f_accuracy, dist
+    return((c_loss, f_loss), 
+           (c_accuracy, f_accuracy), 
+           (enc_init_state, att_init_state, c_init_state, f_init_state), 
+           (encoder_state, att_state, c_state, f_state), 
+           prs)
+
---
+++ train_wave.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy as np
+from indrnn_convattention import build_model
+import datetime, wave, os, struct, linecache
+
+input_data = tf.placeholder(tf.int32, shape=(None, None, 2)) # (B, T, c + s)
+features   = tf.placeholder(tf.int32, shape=(None, None)) # (B, N_f)
+
+BATCH_SIZE = 5
+TIME_STEPS = 1000
+DICT_SIZE  = 30
+
+losses, accuracies, init_states, states_op, prs = build_model(
+    input_data, features,
+    BATCH_SIZE=BATCH_SIZE,
+    DICT_SIZE=DICT_SIZE,
+    TIME_STEPS=TIME_STEPS)
+
+c_loss, f_loss = losses
+c_acc, f_acc = accuracies
+
+LEARNING_RATE_INIT = 2e-4
+LEARNING_RATE_DECAY_STEPS = 100000
+
+global_step = tf.get_variable("global_step", shape=[], trainable=False,
+                              initializer=tf.zeros_initializer, dtype=tf.int32)
+learning_rate = tf.train.exponential_decay(
+    LEARNING_RATE_INIT, global_step,
+    LEARNING_RATE_DECAY_STEPS, 0.1,
+    staircase=True)
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+gradients, variables = zip(*optimizer.compute_gradients(c_loss + f_loss))
+clipped_grads, _ = tf.clip_by_global_norm(gradients, 2.)
+with tf.control_dependencies(update_ops):
+    train_op = optimizer.apply_gradients(zip(clipped_grads, variables), global_step=global_step)
+
+merged = tf.summary.merge([
+    tf.summary.scalar('coarse_loss', c_loss),
+    tf.summary.scalar('fine_loss', f_loss),
+    tf.summary.scalar('total_loss', c_loss + f_loss)])
+
+acc_summary = tf.summary.merge([
+    tf.summary.scalar('coarse_accuracy', c_acc),
+    tf.summary.scalar('fine_accuracy', f_acc),
+    tf.summary.histogram('distribution', prs)])
+
+def gen_batch(lineno=1):
+    WEB_dir = "../data/WEB"
+    sentence_batch = []
+    voice_batch   = []
+    
+    while True:
+       line = linecache.getline(os.path.join(WEB_dir, "transcript.txt"), lineno)
+       if line == '':
+           lineno = 1
+           continue
+
+       lineno += 1
+
+       filename, sentence, duration = line.split('\t')
+
+       vocab = "abcedfghijklmnopqrstuvwxyz"
+       encoded = [vocab.find(ch) for ch in sentence.lower()]
+       encoded = np.array([i if i >= 0 else len(vocab) for i in encoded])
+
+       with wave.open(os.path.join(WEB_dir, filename + '.wav'), 'rb') as wavfile:
+           num_frames = wavfile.getnframes()
+           if wavfile.getnchannels() == 2:
+               fmt = '{}B'.format(num_frames * 4)
+               raw_data = wavfile.readframes(num_frames)
+               voice = np.array(struct.unpack(fmt, raw_data))
+               voice = np.reshape(voice, (-1, 2, 2))[:, 0]
+           else:
+               fmt = '{}B'.format(num_frames * 2)
+               raw_data = wavfile.readframes(num_frames)
+               voice = np.array(struct.unpack(fmt, raw_data))
+               voice = np.reshape(voice, (-1, 2))
+
+       sentence_batch.append(encoded)
+       voice_batch.append(voice)
+
+       if len(sentence_batch) % BATCH_SIZE == 0:
+           max_sentence_len = max(s.shape[0] for s in sentence_batch)
+           orig_sentence_len = [row.shape[0] for row in sentence_batch]
+           sentence_batch = [
+               np.concatenate(
+                    [row, np.zeros((max_sentence_len - row.shape[0]))]
+               ) for row in sentence_batch
+           ]
+           max_voice_len = max(s.shape[0] for s in voice_batch)
+           orig_voice_len = [row.shape[0] for row in voice_batch]
+           voice_batch = [
+               np.concatenate(
+                    [row, np.zeros((max_voice_len - row.shape[0], 2))]
+               ) for row in voice_batch
+           ]
+           yield (np.array(sentence_batch), 
+                  np.array(voice_batch), 
+                  np.array(orig_sentence_len), 
+                  np.array(orig_voice_len))
+
+           sentence_batch = []
+           voice_batch = []
+
+def main():
+    saver = tf.train.Saver()
+
+    with tf.Session(config=tf.ConfigProto()) as sess:
+
+        train_writer = tf.summary.FileWriter('../train_logs/speak', sess.graph)
+
+        sess.run([tf.global_variables_initializer()])
+        if input('Restore Model? ') == 'y':
+            saver.restore(sess, input('Model Path: '))
+            print('Restored. Resuming from iteration', global_step.eval())
+
+        start_time = datetime.datetime.now() 
+        for sentence, data, s_lens, d_lens in gen_batch():
+            
+            states = None
+            for start in range(0, data.shape[1], TIME_STEPS):
+
+                feed_dict = {input_data.name: data[:, start:start + TIME_STEPS],
+                             features.name: sentence[:, start:start + TIME_STEPS]}
+                if states is not None:
+                    feed_dict[states_op] = states
+
+                _, iteration, Lc, Lf, summary, states = sess.run(
+                    [train_op, global_step, c_loss, f_loss, merged, states_op],
+                    feed_dict)
+
+                train_writer.add_summary(summary, iteration)
+
+                if iteration % 10 == 0:
+                    print(iteration, Lc, Lf, Lc + Lf)
+
+                if iteration % 100 == 0:
+                    elapsed = datetime.datetime.now() - start_time
+
+                    print(elapsed / 100 / BATCH_SIZE, "per data point")
+
+                    save_path = saver.save(sess, '../models/speak', iteration)
+                    print('Saved to', save_path)
+
+                    start_time = datetime.datetime.now()
+
+
+if __name__ == "__main__":
+    main()
